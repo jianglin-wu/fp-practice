5a098638cc2ec9ba704af42f0b1bc78cfb22388d
jianglin-wu <wjiangl.id@gmail.com>
2022-05-05 17:59:37 +0800

    feat: chian

--- src/lodash/chian.test.ts
+++ src/lodash/chian.test.ts
@@ -23,3 +23,50 @@ test('[chian] get array object property', () => {
   const countItems = _.chain(arr).map(_.property('count')).value();
   expect(countItems).toEqual([20299, 97961, undefined]);
 });
+
+test('chain count sum', () => {
+  const arr = [
+    {
+      count: 3,
+      name: '1',
+    },
+    {
+      count: 3,
+      name: '1',
+    },
+    {
+      count: 2,
+      name: '2',
+    },
+    {
+      count: 5,
+      name: '3',
+    },
+    {
+      count: 10,
+      name: '2',
+    },
+  ];
+
+  const data = _.chain(arr)
+    .groupBy(_.property('name'))
+    .map((items) => ({
+      name: _.chain(items).head().get('name').value(),
+      count: _.sumBy(items, _.property('count')),
+    }))
+    .value();
+  expect(data).toEqual([
+    {
+      name: '1',
+      count: 6,
+    },
+    {
+      name: '2',
+      count: 12,
+    },
+    {
+      name: '3',
+      count: 5,
+    },
+  ]);
+});
