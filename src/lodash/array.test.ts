210b9905042e726372e06505528e4a7129551a6e
jianglin-wu <wjiangl.id@gmail.com>
2022-05-06 16:06:39 +0800

    feat: add difference case

--- src/lodash/array.test.ts
+++ src/lodash/array.test.ts
@@ -1,5 +1,27 @@
 import * as _ from 'lodash';
 
+test('difference', () => {
+  const a = [1, 2, 3, 4, 5];
+  expect(_.difference(a, [2, 4])).toEqual([1, 3, 5]);
+  expect(a.length).toBe(5);
+});
+
+test('differenceBy', () => {
+  const a = [1, 2, 3, 4, 5];
+  expect(_.differenceBy(a, [2, 4], (n) => n % 2)).toEqual([1, 3, 5]);
+  expect(a.length).toBe(5);
+});
+
+test('differenceWith', () => {
+  const a = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+  expect(
+    _.differenceWith(a, [2, 4], (sourceItem, conditionItem) => {
+      return sourceItem.id === conditionItem;
+    }),
+  ).toEqual([{ id: 1 }, { id: 3 }, { id: 5 }]);
+  expect(a.length).toBe(5);
+});
+
 test('nth', () => {
   const a = [1, 2, 3, 4, 5];
   expect(_.nth(a, 0)).toBe(1);
